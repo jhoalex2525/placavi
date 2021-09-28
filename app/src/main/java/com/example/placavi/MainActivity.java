cc69d7aaec8edc71de238c8c77dc31fd017e8942
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-09-27 23:41:40 -0500

    Se avanza diseño de sitio registrarse

--- app/src/main/java/com/example/placavi/MainActivity.java
+++ app/src/main/java/com/example/placavi/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.placavi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button jbtingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jbtingresar =findViewById(R.id.button);
+    }
+
+    public void Ingresar(View view){
+        Intent intmostrar = new Intent(this, singup.class);
+        startActivity(intmostrar);
     }
 }

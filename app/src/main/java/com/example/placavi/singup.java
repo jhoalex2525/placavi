e0aff7fcf3e099276e209eed08f9e4c14bd85fce
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-10-05 15:03:50 -0500

    Arreglo de imagenes

--- app/src/main/java/com/example/placavi/singup.java
+++ app/src/main/java/com/example/placavi/singup.java
@@ -3,6 +3,7 @@ package com.example.placavi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class singup extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class singup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+    }
+
+    public void vendedor(View view){
+
     }
 }

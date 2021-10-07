90e145e7a68c254ae9b59ddf521aa4469fc2af7f
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-10-07 16:17:49 -0500

    Se arreglan vistas e imagenes

--- app/src/main/java/com/example/placavi/singup.java
+++ app/src/main/java/com/example/placavi/singup.java
@@ -2,18 +2,40 @@ package com.example.placavi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class singup extends AppCompatActivity {
+
+    RadioButton jrbseller, jrbuser, jrbadmin;
+    EditText jetstorename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        jrbseller = findViewById(R.id.rbseller);
+        jrbuser = findViewById(R.id.rbuser);
+        jrbadmin = findViewById(R.id.rbadmin);
+        jetstorename = findViewById(R.id.etstorename);
+
     }
 
-    public void vendedor(View view){
+    public void storeName(View view){
+        if (jrbseller.isChecked()){
+            jetstorename.setVisibility(View.VISIBLE);
+        }
+        else{
+            jetstorename.setVisibility(View.GONE);
+        }
+    }
 
+    public void home (View view){
+        Intent inthome = new Intent(this, MainActivity.class);
+        startActivity(inthome);
     }
 }

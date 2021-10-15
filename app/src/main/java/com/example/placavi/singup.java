8d9e2f571e45f5481156f8050af9926db54314ff
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-10-14 19:08:19 -0500

    Validación de campos llenos para registro de usuario

--- app/src/main/java/com/example/placavi/singup.java
+++ app/src/main/java/com/example/placavi/singup.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -12,7 +13,8 @@ import android.widget.Toast;
 public class singup extends AppCompatActivity {
 
     RadioButton jrbseller, jrbuser, jrbadmin;
-    EditText jetstorename;
+    EditText jetstorename, jetregistername, jetregisteremail, jetregistercountry, jetregistercity, jetregisterpassword;
+    Button jbtnnewregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,35 +25,43 @@ public class singup extends AppCompatActivity {
         jrbuser = findViewById(R.id.rbuser);
         jrbadmin = findViewById(R.id.rbadmin);
         jetstorename = findViewById(R.id.etstorename);
+        jetregistername = findViewById(R.id.etregistername);
+        jetregisteremail = findViewById(R.id.etregisteremail);
+        jetregistercountry = findViewById(R.id.etregistercountry);
+        jetregistercity = findViewById(R.id.etregistercity);
+        jetregisterpassword = findViewById(R.id.etregisterpassword);
+        jbtnnewregister = findViewById(R.id.btnnewregister);
 
     }
 
     public void storeName(View view){
         if (jrbseller.isChecked()){
             jetstorename.setVisibility(View.VISIBLE);
+            jetstorename.setText("");
         }
         else{
             jetstorename.setVisibility(View.GONE);
+            jetstorename.setText("No es Negocio");
         }
     }
 
     public void singUpFieldValidations (View view){
-        String email, password;
-        email = jetloginemail.getText().toString();
-        password = jetloginpassword.getText().toString();
-        if (email.isEmpty()){
-            Toast.makeText(this, "Email es requerido", Toast.LENGTH_SHORT).show();
-            jetloginemail.requestFocus();
+        String storename, registername, registeremail, registercountry, registercity, registerpassword;
+        storename = jetstorename.getText().toString();
+        registername = jetregistername.getText().toString();
+        registeremail = jetregisteremail.getText().toString();
+        registercountry = jetregistercountry.getText().toString();
+        registercity = jetregistercity.getText().toString();
+        registerpassword = jetregisterpassword.getText().toString();
+        if (storename.isEmpty()||registername.isEmpty()||registeremail.isEmpty()||registercountry.isEmpty()||registercity.isEmpty()||registerpassword.isEmpty()){
+            Toast.makeText(this, "Todos los campos deben ser diligenciados", Toast.LENGTH_SHORT).show();
         }
-        else {
-            if (password.isEmpty()){
-                Toast.makeText(this, "Contraseña es requerida", Toast.LENGTH_SHORT).show();
-                jetloginpassword.requestFocus();
-            }
-            else {
-                Toast.makeText(this, "Su cuenta ha sido creada", Toast.LENGTH_SHORT).show();
-                Intent intwelcome = new Intent(this, MainActivity.class);
-                startActivity(intwelcome);
-            }
+        else{
+            Toast.makeText(this, "Su cuenta ha sido creada", Toast.LENGTH_SHORT).show();
+            Intent intwelcome = new Intent(this, MainActivity.class);
+            startActivity(intwelcome);
         }
+    }
+
+
 }

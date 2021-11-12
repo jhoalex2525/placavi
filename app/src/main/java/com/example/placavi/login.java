45acd34f4cb5c88a050a5b42dbfeebf31a6fb32b
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-11-11 19:33:19 -0500

    Validaciones email y contraseña del login

--- app/src/main/java/com/example/placavi/login.java
+++ app/src/main/java/com/example/placavi/login.java
@@ -1,6 +1,7 @@
 package com.example.placavi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,24 +25,37 @@ public class login extends AppCompatActivity {
         jbtnloginapp = findViewById(R.id.btnloginapp);
     }
 
-    public void loginFieldValidations (View view){
-        String email, password;
+    public void loginEmailFieldValidations (){
+        String email;
         email = jetloginemail.getText().toString();
-        password = jetloginpassword.getText().toString();
         if (email.isEmpty()){
             Toast.makeText(this, "Email es requerido", Toast.LENGTH_SHORT).show();
             jetloginemail.requestFocus();
         }
         else {
-            if (password.isEmpty()){
+            if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(this, "Inserte un formato de email válido", Toast.LENGTH_SHORT).show();
+                jetloginemail.requestFocus();
+            }
+        }
+    }
+
+    public void loginPasswordFieldValidations (){
+        String password;
+        password = jetloginpassword.getText().toString();
+        if (password.isEmpty()){
                 Toast.makeText(this, "Contraseña es requerida", Toast.LENGTH_SHORT).show();
                 jetloginpassword.requestFocus();
-            }
-            else {
+        }
+        else {
                 Toast.makeText(this, "Bienvenido, estas logueado", Toast.LENGTH_SHORT).show();
                 Intent intwelcome = new Intent(this, MainActivity.class);
                 startActivity(intwelcome);
-            }
         }
+    }
+
+    public void loginFieldValidations(View view){
+        loginEmailFieldValidations();
+        loginPasswordFieldValidations();
     }
 }
--- app/src/main/java/com/example/placavi/singup.java
+++ app/src/main/java/com/example/placavi/singup.java
@@ -64,4 +64,6 @@ public class singup extends AppCompatActivity {
     }
 
 
+
+
 }

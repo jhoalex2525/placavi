b468d0b491405c4d4b0d77abb67a10a12631491e
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-11-18 19:44:23 -0500

    Validaciones campos de logueo

--- app/src/main/java/com/example/placavi/login.java
+++ app/src/main/java/com/example/placavi/login.java
@@ -10,47 +10,57 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.placavi.databinding.ActivityLoginBinding;
+
+import java.util.regex.Pattern;
+
 public class login extends AppCompatActivity {
 
-    EditText jetloginemail, jetloginpassword;
-    Button jbtnloginapp;
+    private ActivityLoginBinding activityLoginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        jetloginemail = findViewById(R.id.etloginemail);
-        jetloginpassword = findViewById(R.id.etloginpassword);
-        jbtnloginapp = findViewById(R.id.btnloginapp);
+        activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = activityLoginBinding.getRoot();
+        setContentView(view);
     }
 
     public void loginEmailFieldValidations (){
-        String email;
-        email = jetloginemail.getText().toString();
+        String email = activityLoginBinding.etloginemail.getText().toString();
         if (email.isEmpty()){
             Toast.makeText(this, "Email es requerido", Toast.LENGTH_SHORT).show();
-            jetloginemail.requestFocus();
         }
         else {
             if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
                 Toast.makeText(this, "Inserte un formato de email válido", Toast.LENGTH_SHORT).show();
-                jetloginemail.requestFocus();
             }
         }
     }
 
     public void loginPasswordFieldValidations (){
-        String password;
-        password = jetloginpassword.getText().toString();
+        String password = activityLoginBinding.etloginpassword.getText().toString();
+        Pattern passwordpattern = Pattern.compile(
+                "^"+
+                        "(?=.*[A-Z])"+      //1 upper letter
+                        "(?=.*[@#$%&^+=])"+ //1 especial character
+                        "(?=\\S+$)"+        //No white spaces
+                        ".{8,}"+            //At lets 8 characters
+                "$"
+        );
         if (password.isEmpty()){
                 Toast.makeText(this, "Contraseña es requerida", Toast.LENGTH_SHORT).show();
-                jetloginpassword.requestFocus();
         }
         else {
+            if (!passwordpattern.matcher(password).matches())
+            {
+                Toast.makeText(this, "Inserte un formato de contraseña válido", Toast.LENGTH_SHORT).show();
+            }
+            else {
                 Toast.makeText(this, "Bienvenido, estas logueado", Toast.LENGTH_SHORT).show();
                 Intent intwelcome = new Intent(this, MainActivity.class);
                 startActivity(intwelcome);
+            }
         }
     }
 
--- app/src/main/java/com/example/placavi/singup.java
+++ app/src/main/java/com/example/placavi/singup.java
@@ -1,6 +1,7 @@
 package com.example.placavi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,60 +11,97 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.example.placavi.databinding.ActivitySingupBinding;
+
+import java.util.regex.Pattern;
+
 public class singup extends AppCompatActivity {
 
-    RadioButton jrbseller, jrbuser, jrbadmin;
-    EditText jetstorename, jetregistername, jetregisteremail, jetregistercountry, jetregistercity, jetregisterpassword;
-    Button jbtnnewregister;
+    private ActivitySingupBinding activitySingupBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_singup);
-
-        jrbseller = findViewById(R.id.rbseller);
-        jrbuser = findViewById(R.id.rbuser);
-        jrbadmin = findViewById(R.id.rbadmin);
-        jetstorename = findViewById(R.id.etstorename);
-        jetregistername = findViewById(R.id.etregistername);
-        jetregisteremail = findViewById(R.id.etregisteremail);
-        jetregistercountry = findViewById(R.id.etregistercountry);
-        jetregistercity = findViewById(R.id.etregistercity);
-        jetregisterpassword = findViewById(R.id.etregisterpassword);
-        jbtnnewregister = findViewById(R.id.btnnewregister);
-
+        activitySingupBinding = ActivitySingupBinding.inflate(getLayoutInflater());
+        View view = activitySingupBinding.getRoot();
+        setContentView(view);
     }
 
     public void storeName(View view){
-        if (jrbseller.isChecked()){
-            jetstorename.setVisibility(View.VISIBLE);
-            jetstorename.setText("");
+        String storename = activitySingupBinding.etstorename.getText().toString();
+        if (activitySingupBinding.rbseller.isChecked()){
+            activitySingupBinding.etstorename.setVisibility(View.VISIBLE);
+            activitySingupBinding.etstorename.setText("");
         }
         else{
-            jetstorename.setVisibility(View.GONE);
-            jetstorename.setText("No es Negocio");
+            activitySingupBinding.etstorename.setVisibility(View.GONE);
+            activitySingupBinding.etstorename.setText("No es Negocio");
         }
     }
 
-    public void singUpFieldValidations (View view){
-        String storename, registername, registeremail, registercountry, registercity, registerpassword;
-        storename = jetstorename.getText().toString();
-        registername = jetregistername.getText().toString();
-        registeremail = jetregisteremail.getText().toString();
-        registercountry = jetregistercountry.getText().toString();
-        registercity = jetregistercity.getText().toString();
-        registerpassword = jetregisterpassword.getText().toString();
-        if (storename.isEmpty()||registername.isEmpty()||registeremail.isEmpty()||registercountry.isEmpty()||registercity.isEmpty()||registerpassword.isEmpty()){
+    public boolean singUpFieldValidations (){
+        String storename = activitySingupBinding.etstorename.getText().toString();
+        String registername = activitySingupBinding.etregistername.getText().toString();
+        String registercountry = activitySingupBinding.etregistercountry.getText().toString();
+        String registercity = activitySingupBinding.etregistercity.getText().toString();
+
+        if (storename.isEmpty()||registername.isEmpty()||registercountry.isEmpty()||registercity.isEmpty()){
             Toast.makeText(this, "Todos los campos deben ser diligenciados", Toast.LENGTH_SHORT).show();
+            return false;
         }
-        else{
+        else {
+            return true;
+        }
+    }
+
+    public boolean singupEmailFieldValidations (){
+        String email = activitySingupBinding.etregisteremail.getText().toString();
+        if (email.isEmpty()){
+            Toast.makeText(this, "Email es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(this, "Inserte un formato de email válido", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+    public boolean singupPasswordFieldValidations (){
+        String password = activitySingupBinding.etregisterpassword.getText().toString();
+        Pattern passwordpattern = Pattern.compile(
+                "^"+
+                        "(?=.*[A-Z])"+      //1 upper letter
+                        "(?=.*[@#$%&^+=])"+ //1 especial character
+                        "(?=\\S+$)"+        //No white spaces
+                        ".{8,}"+            //At lets 8 characters
+                        "$"
+        );
+        if (password.isEmpty()){
+            Toast.makeText(this, "Contraseña es requerida", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            if (!passwordpattern.matcher(password).matches())
+            {
+                Toast.makeText(this, "Inserte un formato de contraseña válido", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+
+    public void singupValidations(View view){
+        if( singUpFieldValidations()&&singupEmailFieldValidations()&&singupPasswordFieldValidations()){
             Toast.makeText(this, "Su cuenta ha sido creada", Toast.LENGTH_SHORT).show();
             Intent intwelcome = new Intent(this, MainActivity.class);
             startActivity(intwelcome);
         }
     }
-
-
-
-
 }
